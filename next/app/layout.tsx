88ebaa4f3ae51c048e3fa728e8c93193c993ec5b
mrpbj3 <therealprodbyjo@gmail.com>
2025-11-13 12:30:55 -0500

    Add metadata and wrap children in QueryClientProvider

--- next/app/layout.tsx
+++ next/app/layout.tsx
@@ -1,12 +1,28 @@
+// app/layout.tsx
+import type { Metadata } from "next";
 import "./globals.css";
-import { AuthProvider } from "@/auth/AuthProvider";
 
-export default function RootLayout({ children }) {
+import { AuthProvider } from "@/auth/AuthProvider";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "@/lib/queryClient";
+
+export const metadata: Metadata = {
+  title: "PBJ Lyfe",
+  description: "One simple daily flow to track your Lyfe",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <body>
         <AuthProvider>
-          {children}
+          <QueryClientProvider client={queryClient}>
+            {children}
+          </QueryClientProvider>
         </AuthProvider>
       </body>
     </html>
